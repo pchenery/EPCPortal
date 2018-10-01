50b1ceaf6312047c0a624d44a9442bb4f1d569e5
Aoife Pearson <aoifejo.pearson@googlemail.com>
2018-10-01 14:44:33 +0100

    added reference to EPCPortal project in the unit test project so that relevant unit tests can be created

---
+++ EPCPortalTesting/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EPCPortal;
+
+namespace EPCPortalTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
