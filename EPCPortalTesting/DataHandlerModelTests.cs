31c27e463b22e747dabfff2c399e7efa54b3c0cf
Aoife Pearson <aoifejo.pearson@googlemail.com>
2018-10-02 12:00:21 +0100

    Unit tests for functionality regarding searching for addresses by postcode, and returning 10 or less results

--- EPCPortalTesting/DataHandlerModelTests.cs
+++ EPCPortalTesting/DataHandlerModelTests.cs
@@ -9,11 +9,8 @@ namespace EPCPortalTesting
     public class DataHandlerModelTests
     {
         DataHandlerModel dataHandlerModel = new DataHandlerModel();
-       
-        [TestMethod]
-        public void SearchForAddressByHouseNumber()
-        {
-            List<string> MockAPIReturn = new List<string>()
+
+        List<string> MockAPIReturn = new List<string>()
             {
                 "1 fake street",
                 "2 fake street",
@@ -22,9 +19,44 @@ namespace EPCPortalTesting
                 "5 fake street"
             };
 
+        List<string> GetListOfAddressesByPostcode(string postcode)
+        {
+            List<string> ListOfAddresses = new List<string>();
+            //API Call
+            ListOfAddresses = MockAPIReturn;
+            return ListOfAddresses;
+        }
+
+        [TestMethod]
+        public void SearchForAddressesByPostcode()
+        {
+            List<string> addresses = GetListOfAddressesByPostcode("RM96BF");
+            Assert.IsNotNull(addresses);
+        }
+
+        [TestMethod]
+        public void SearchForAddressesByPostcodeDoesNotReturnMoreThan10Results()
+        {
+            List<string> addresses = GetListOfAddressesByPostcode("RM96BF");
+            bool fail = true;
+            if (addresses.Count > 10)
+            {
+                fail = true;
+            }
+            else
+            {
+                fail = false;
+            }
+            Assert.IsFalse(fail);
+        }
+
+        [TestMethod]
+        public void SearchForAddressByHouseNumber()
+        {
             List<string> newlist = dataHandlerModel.FilterAddressesByHouseNumber(3, MockAPIReturn);
 
             Assert.IsNotNull(newlist);
         }
+
     }
 }
