35f08eeae0bd6c45a283e00e9aa0fd890c01f565
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-10-02 12:49:49 +0100

    EPC API caller class added with one passing integration test.

---
+++ EPCData.API/EpcDataApiCallerService.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace EPCData.API
+{
+    public class EpcDataApiCallerService : IEpcDataApiCallerService
+    {
+        private const string EpcDataApiUrl = "https://dceas-user-site-staging.cloudapps.digital/api/epc";
+        private readonly IRestClient client;
+
+        public EpcDataApiCallerService()
+        {
+            client = new RestClient { BaseUrl = new Uri(EpcDataApiUrl) };
+        }
+
+        public async Task<T> ExecuteRequestAsync<T>(RequestParameters requestParameters)
+        {
+            var request = new RestRequest();
+            request.AddParameter(requestParameters.Postcode.Name, requestParameters.Postcode.Value);
+            request.AddParameter(requestParameters.Size.Name, requestParameters.Size.Value);
+
+            var restResponse = await client.ExecuteTaskAsync<T>(request);
+            
+            return restResponse.Data;
+        }
+    }
+}
---
+++ EPCPortalTesting/EPCData.API.IntegationTests/EpcDataApiCallerServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using EPCData.API;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace EPCPortalTesting.EPCData.API.IntegationTests
+{
+    [TestFixture]
+    public class EpcDataApiCallerServiceTests
+    {
+        private IEpcDataApiCallerService service;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            service = new EpcDataApiCallerService();
+        }
+
+        [Test]
+        public async Task ExecuteRequestAsync_GivenValidPostcodeAndSizeValue_ThenDataReturnedIsNotNull()
+        {
+            const string postcode = "NW5 2TA";
+            const int size = 1;
+            var parameters = new RequestParameters(postcode, size);
+
+            var data = await service.ExecuteRequestAsync<string>(parameters);
+
+            data.Should().NotBeNullOrEmpty();
+        }
+    }
+}
