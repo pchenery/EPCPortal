db57a77f33495d021a7c5f3d35a3a61e29f14139
Zabeen Patel <Zabeen.Patel@anthonynolan.org>
2018-10-02 11:43:49 +0100

    RequestParameters unit tests added (some failing).

---
+++ EPCPortalTesting/EPCData.API.UnitTests/RequestParametersTests.cs
@@ -0,0 +1,61 @@
+﻿using EPCData.API;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace EPCPortalTesting.EPCData.API.UnitTests
+{
+    [TestFixture]
+    public class RequestParametersTests
+    {
+        [TestCase("")]
+        [TestCase(null)]
+        public void CreateNew_WhenPostcodeEmptyOrNull_ThenExceptionThrown(string postcodeValue)
+        {
+            const int sizeValue = 1;
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var requestParameters = new RequestParameters(postcodeValue, sizeValue);
+            });
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(101)]
+        public void CreateNew_WhenSizeNotBetween1And100_ThenExceptionThrown(int sizeValue)
+        {
+            const string postcodeValue = "postcode";
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var requestParameters = new RequestParameters(postcodeValue, sizeValue);
+            });
+        }
+
+        [Test]
+        public void CreateNew_WhenPostcodeProvidedAndSizeBetween1And100_ThenNoExceptionThrown()
+        {
+            const string postcodeValue = "postcode";
+            const int sizeValue = 1;
+
+            Assert.DoesNotThrow(() =>
+            {
+                var requestParameters = new RequestParameters(postcodeValue, sizeValue);
+            });
+        }
+
+        [TestCase("ABC 123")]
+        [TestCase(" ABC123")]
+        [TestCase("ABC123 ")]
+        public void CreateNew_WhenPostcodeProvidedContainingSpaces_ThenSpaceIsRemoved(string postcodeValue)
+        {
+            const string expectedPostcodeValue = "ABC123";
+            const int sizeValue = 1;
+
+            var requestParameters = new RequestParameters(postcodeValue, sizeValue);
+
+            requestParameters.Postcode.Should().Be(expectedPostcodeValue);
+        }
+    }
+}
