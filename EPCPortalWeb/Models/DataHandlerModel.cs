7fd7840a8574dc7e1852519aa27141eaab949e7b
Aoife Pearson <aoifejo.pearson@googlemail.com>
2018-10-02 10:05:13 +0100

    created datahandler model to capture usable data once the API is implemeneted, added postcode textbox that will use the data model to set the postcode value

---
+++ EPCPortalWeb/Models/DataHandlerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EPCPortalWeb.Models
+{
+    public class DataHandlerModel
+    {
+        public string Postcode { get; set; }
+        public int HouseNumber { get; set; }
+    }
+}
